d41e4f62ca3c22f76808cfc539182138bc245260
dharshini <dharshinivk30@gmail.com>
2024-09-20 11:42:08 +0530

    amstrong_number

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Program to check if a number is prime or not
 
 num = 29
@@ -24,3 +25,25 @@ elif num > 1:
         print(num, "is not a prime number")
     else:
         print(num, "is a prime number")
+=======
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+>>>>>>> Stashed changes
