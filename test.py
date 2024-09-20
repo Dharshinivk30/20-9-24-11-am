579ce96dfcd1419e6ddf0db0c454e256f2e4a8df
dharshini <dharshinivk30@gmail.com>
2024-09-20 11:40:40 +0530

    check_prime_number

--- test.py
+++ test.py
@@ -1,6 +1,26 @@
-# Program to generate a random number between 0 and 9
+# Program to check if a number is prime or not
 
-# importing the random module
-import random
+num = 29
 
-print(random.randint(0,9))
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
